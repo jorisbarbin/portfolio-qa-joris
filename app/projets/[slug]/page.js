54c953832c598dbe492206f5a291ac93321c9ace
Joris Barbin <barbin.joris@hotmail.fr>
2026-05-13 15:06:42 +0200

    Add dynamic project pages

---
+++ app/projets/[slug]/page.js
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import projectsData from "../../../data/projects.json";
+
+export default async function ProjectDetail({ params }) {
+  const { slug } = await params;
+
+  const project = projectsData.find((project) => project.slug === slug);
+
+  if (!project) {
+    notFound();
+  }
+
+  return (
+    <section className="pageSection">
+      <div className="pageContainer">
+        <Link href="/projets" className="backLink">
+          ← Retour aux projets
+        </Link>
+
+        <div className="projectDetailHeader">
+          <span className="projectCategory">{project.category}</span>
+          <span className="projectStatus">{project.status}</span>
+        </div>
+
+        <h1 className="pageTitle">{project.title}</h1>
+
+        <p className="pageText">{project.longDescription}</p>
+
+        <div className="projectTags detailTags">
+          {project.tags.map((tag) => (
+            <span key={tag} className="projectTag">
+              {tag}
+            </span>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export function generateStaticParams() {
+  return projectsData.map((project) => ({
+    slug: project.slug,
+  }));
+}
